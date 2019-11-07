5f4ad6218075463a806f8fe3e9dd6253820183c2
Katherine Mello <katherine.mello8@gmail.com>
2019-11-07 04:44:25 -0500

    Typescript & webpack config setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+module.exports = {
+  mode: 'development',
+
+  entry: './index.tsx',
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  }
+}
